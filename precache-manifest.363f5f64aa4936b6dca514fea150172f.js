81e03b37256a84c54c1e6f5fadc86237e03f6a00
jhserodio <joao.serodio@vnator.com>
2020-09-15 10:31:01 -0300

    Updates

---
+++ precache-manifest.363f5f64aa4936b6dca514fea150172f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8259526954c42136bd6bcdae5cc0d1a",
+    "url": "/index.html/index.html"
+  },
+  {
+    "revision": "c5dbd55e8a208e453272",
+    "url": "/index.html/static/css/main.5d01af78.chunk.css"
+  },
+  {
+    "revision": "115fc7fe182ea3427038",
+    "url": "/index.html/static/js/2.50f4a800.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/index.html/static/js/2.50f4a800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5dbd55e8a208e453272",
+    "url": "/index.html/static/js/main.f95a5a25.chunk.js"
+  },
+  {
+    "revision": "fc850299a20ad7ee9d34",
+    "url": "/index.html/static/js/runtime-main.0a511a5d.js"
+  },
+  {
+    "revision": "c05cfcea66591be5dac8e1b9c3223264",
+    "url": "/index.html/static/media/eu.c05cfcea.jpeg"
+  },
+  {
+    "revision": "010b604f84431eb7cdbc6620aa64cb55",
+    "url": "/index.html/static/media/firacode-bold.010b604f.woff2"
+  },
+  {
+    "revision": "22cf339e034bb9a3d21c0f7c6c11fade",
+    "url": "/index.html/static/media/firacode-bold.22cf339e.woff"
+  },
+  {
+    "revision": "f49305f3ff86f0f33e04658fa795c95c",
+    "url": "/index.html/static/media/firacode-bold.f49305f3.otf"
+  },
+  {
+    "revision": "004ca30540f4e610737895585f14a6b5",
+    "url": "/index.html/static/media/firacode-light.004ca305.woff"
+  },
+  {
+    "revision": "bc4d306ac11284a7d5460b426f3ff739",
+    "url": "/index.html/static/media/firacode-light.bc4d306a.otf"
+  },
+  {
+    "revision": "f8f87b9826c9d68e6666cc483bd0451c",
+    "url": "/index.html/static/media/firacode-light.f8f87b98.woff2"
+  },
+  {
+    "revision": "551a04f3a6544dcb9468992a3d87a2b0",
+    "url": "/index.html/static/media/firacode-medium.551a04f3.woff"
+  },
+  {
+    "revision": "7064d44fb7d3128f4958b8b907ef1d83",
+    "url": "/index.html/static/media/firacode-medium.7064d44f.otf"
+  },
+  {
+    "revision": "f18fd1b3cc4fc5265dc28c68947294a4",
+    "url": "/index.html/static/media/firacode-medium.f18fd1b3.woff2"
+  }
+]);
